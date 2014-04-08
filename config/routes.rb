b4944abcf28426feeed60bc102eef3e987d6abc0
= <thomas07@vt.edu>
2014-04-08 13:35:17 -0700

    more tests...

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root :to => "/"
+  root to: 'pages#main'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
